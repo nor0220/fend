dbd641c95a79c4b8813420b87f7ae1ce35d741a4
nmaraz <nora.maraz@nng.com>
2022-01-26 15:03:55 +0100

    basics from the project start page

--- projects/evaluate-news-nlp/src/client/js/formHandler.js
+++ projects/evaluate-news-nlp/src/client/js/formHandler.js
@@ -1,16 +1,36 @@
+const dotenv = require('dotenv');
+dotenv.config();
+
+const API = 'https://api.meaningcloud.com/sentiment-2.1';
+const API_key = process.env.API_KEY;
+
+console.log(`Your API key is ${API_key}`);
+
 function handleSubmit(event) {
-    event.preventDefault()
+    event.preventDefault();
 
     // check what text was put into the form field
-    let formText = document.getElementById('name').value
-    checkForName(formText)
+    let formText = document.getElementById('name').value;
+    //Client.checkForName(formText)
 
-    console.log("::: Form Submitted :::")
-    fetch('http://localhost:8080/test')
-    .then(res => res.json())
-    .then(function(res) {
-        document.getElementById('results').innerHTML = res.message
+    const formdata = new FormData();
+    formdata.append("key", API_key);
+    formdata.append("txt", formText);
+    formdata.append("lang", "en");
+
+    const requestOptions = {
+    method: 'POST',
+    body: formdata,
+    redirect: 'follow'
+    };
+
+    console.log("::: Form Submitted :::");
+    const response = fetch(API, requestOptions)
+    .then(response => response.json())
+    .then(function(response) {
+        document.getElementById('results').innerHTML = response.message
     })
+    .catch(error => console.log('error', error));
 }
 
 export { handleSubmit }
--- projects/evaluate-news-nlp/src/server/index.js
+++ projects/evaluate-news-nlp/src/server/index.js
@@ -4,13 +4,23 @@ const mockAPIResponse = require('./mockAPI.js')
 
 const app = express()
 
+/* Middleware*/
+const bodyParser = require('body-parser');
+//Here we are configuring express to use body-parser as middle-ware.
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// Cors for cross origin allowance
+const cors = require('cors');
+app.use(cors());
+
 app.use(express.static('dist'))
 
 console.log(__dirname)
 
 app.get('/', function (req, res) {
-    // res.sendFile('dist/index.html')
-    res.sendFile(path.resolve('src/client/views/index.html'))
+    res.sendFile('dist/index.html')
+    //res.sendFile(path.resolve('src/client/views/index.html'))
 })
 
 // designates what port the app will listen to for incoming requests
